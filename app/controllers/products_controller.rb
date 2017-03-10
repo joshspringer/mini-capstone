28bb9df02ae723736b73f5ecdb47a18998d73b3a
Josh <jspringxc@gmail.com>
2017-03-09 21:16:22 -0500

    turn routes RESTful, create make, show, and index actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,18 @@
 class ProductsController < ApplicationController
-
-  def all_products
+  def index
     @products = Product.all
-    render "all_products.html.erb"
+    render "index.html.erb"
   end
 
+  def query_params_method
+    @parameter = params
+    # p params['joe']
+    render "query_params_view.html.erb"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    render "show.html.erb"
+  end
 end
