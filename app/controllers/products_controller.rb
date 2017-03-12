26d1c24fc17e89cffd51882d59aff4b1233c03c4
Josh <jspringxc@gmail.com>
2017-03-12 10:28:05 -0400

    create edit form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,23 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render "index.html.erb"
+    render 'index.html.erb'
   end
 
   def query_params_method
     @parameter = params
     # p params['joe']
-    render "query_params_view.html.erb"
+    render 'query_params_view.html.erb'
   end
 
   def show
     @product = Product.find_by(id: params[:id])
 
-    render "show.html.erb"
+    render 'show.html.erb'
   end
 
   def new
-    render "new.html.erb"
+    render 'new.html.erb'
   end
 
   def create
@@ -27,9 +27,25 @@ class ProductsController < ApplicationController
       brand: params['form_brand'],
       description: params['form_description'],
       price: params['form_price']
-      )
+    )
     @product.save
     # show a view with the info the user entered
     render 'create.html.erb'
+  end
+
+  def edit
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params['id'])
+    @product.update(
+      name: params['form_name'],
+      brand: params['form_brand'],
+      description: params['form_description'],
+      price: params['form_price']
+    )
+    @product.save
+    render 'update.html.erb'
   end
 end
